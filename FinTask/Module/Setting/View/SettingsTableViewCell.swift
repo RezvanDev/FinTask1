4c8b8f0510e897441404675c0b39259e5dbe28d8
VedroMan <123834638+VedroMan@users.noreply.github.com>
2024-06-30 19:15:09 +0300

    Add ViewController to settings

---
+++ FinTask/Module/Setting/View/SettingsTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsTableViewCell.swift
+//  FinTask
+//
+//  Created by Timofey on 30.06.24.
+//
+
+import Foundation
---
+++ FinTask/Module/Setting/ViewController/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  FinTask
+//
+//  Created by Timofey on 30.06.24.
+//
+
+import Foundation
