39fb59a37884f4e23c50411ea966d2bb190a2a01
VedroMan <123834638+VedroMan@users.noreply.github.com>
2024-07-04 18:45:41 +0300

    add settings view

--- FinTask/Module/Setting/View/SettingsTableViewCell.swift
+++ FinTask/Module/Setting/View/SettingsTableViewCell.swift
@@ -11,33 +11,58 @@ class SettingsTableViewCell: UITableViewCell, CellProtocols {
     
     static var reuseId: String = "SettingsTableViewCell"
     
-    //UI elements
-    private lazy var imageIcon: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "crown")
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
+    private lazy var iconImage: UIImageView = {
+        let icon = UIImageView()
+        icon.image = UIImage(systemName: "folder")
+        icon.tintColor = .black
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
     }()
-    private lazy var vStack: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .vertical
-        stack.spacing = 5
-        stack.alignment = .leading
-        stack.distribution = .fill
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        return stack
+    
+    private lazy var titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Test"
+        lbl.font = .systemFont(ofSize: 20, weight: .bold)
+        lbl.textColor = .black
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setup(image: UIImage?, title: String) {
-        //setup the elements
+    private func setupCell() {
+        [iconImage, titleLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+        backgroundColor = AppColors.lightGrayMain
+        // 1  Нужно чтобы были не ячейки а секции и в каждой секции ячейка
+        NSLayoutConstraint.activate([
+            
+            //title label constraints
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: 80),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            
+            //iconImage constraints
+            iconImage.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            iconImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            iconImage.heightAnchor.constraint(equalToConstant: 35),
+            iconImage.widthAnchor.constraint(equalToConstant: 35)
+        ])
+    }
+    
+    func configure(contact: (image: UIImage?, title: String)) {
+        iconImage.image = contact.image
+        titleLabel.text = contact.title
+        
     }
     
 }
--- FinTask/Module/Setting/ViewController/SettingsViewController.swift
+++ FinTask/Module/Setting/ViewController/SettingsViewController.swift
@@ -7,14 +7,16 @@
 
 import UIKit
 
-class SettingsViewController: UITableViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private var tapGesture: UITapGestureRecognizer?
     
     private lazy var settingsCell: [(image: UIImage?, title: String)] = [
-        (UIImage(systemName: "wallet"),"Категории расходов"),
-        (UIImage(systemName: "chart.bar"),"Категории расходов"),
+        (UIImage(systemName: "square.and.arrow.up"),"Категории расходов"),
+        (UIImage(systemName: "square.and.arrow.down"),"Категории доходов"),
         (UIImage(systemName: "dollarsign.circle"),"Валюта по умолчанию"),
         (UIImage(systemName: "globe"),"Язык интерфейса"),
-        (UIImage(systemName: "sliders.horizontal"),"Дополнительно")
+        (UIImage(named: "sliders"),"Дополнительно")
     ]
     
     override func viewDidLoad() {
@@ -22,34 +24,191 @@ class SettingsViewController: UITableViewController {
         
         view.backgroundColor = .white
         view.addSubview(settingsTitle)
+        view.addSubview(tableView)
+        view.addSubview(mainView)
+        view.addSubview(clearMainView)
+        view.addSubview(supportButton)
+        mainView.addSubview(premiumStackView)
+        premiumStackView.addArrangedSubview(crownImageView)
+        premiumStackView.addArrangedSubview(premiumLabel)
         
+        //MARK: --Constraints
         //settingsTitle constraints
         NSLayoutConstraint.activate([
-            settingsTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            settingsTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+            settingsTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            settingsTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            //table constraints
+            tableView.topAnchor.constraint(equalTo: settingsTitle.bottomAnchor, constant: 200),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.heightAnchor.constraint(equalToConstant: 500),
+            
+            //mainView constraints
+            mainView.topAnchor.constraint(equalTo: settingsTitle.bottomAnchor, constant: 16),
+            mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            mainView.heightAnchor.constraint(equalToConstant: 150),
+            
+            //stackView constraints
+            premiumStackView.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 20),
+            premiumStackView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 15),
+            premiumStackView.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -16),
+            premiumStackView.bottomAnchor.constraint(equalTo: mainView.bottomAnchor, constant: -20),
+            premiumStackView.heightAnchor.constraint(equalToConstant: 100),
+            premiumStackView.widthAnchor.constraint(equalToConstant: 120),
+            
+            //crownImage constraints
+            crownImageView.heightAnchor.constraint(equalToConstant: 10),
+            crownImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            //clearMainView constraints
+            clearMainView.topAnchor.constraint(equalTo: view.topAnchor),
+            clearMainView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            clearMainView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            clearMainView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            //supportButton constraints
+            supportButton.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 20),
+            supportButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            supportButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            supportButton.heightAnchor.constraint(equalToConstant: 50),
+            supportButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
         ])
-        //regestration of cell
-        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: "SettingsCell")
+        
     }
+    
+    //MARK: --ViewSetup
     //main settings title
     private lazy var settingsTitle: UILabel = {
         let lbl = UILabel()
-        lbl.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        lbl.font = UIFont.systemFont(ofSize: 30, weight: .bold)
         lbl.text = "Настройки"
         lbl.textColor = .black
         lbl.translatesAutoresizingMaskIntoConstraints = false
         return lbl
     }()
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
-        return settingsCell.count
+    private lazy var tableView: UITableView = {
+        let tbView = UITableView()
+        tbView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.reuseId)
+        tbView.delegate = self
+        tbView.dataSource = self
+        tbView.translatesAutoresizingMaskIntoConstraints = false
+        tbView.layer.cornerRadius = 20
+        tbView.isScrollEnabled = false
+        return tbView
+    }()
+    
+    //setup mainView
+    private lazy var mainView: UIView = {
+        let mainObj = UIView()
+        mainObj.backgroundColor = AppColors.mainGreen
+        mainObj.translatesAutoresizingMaskIntoConstraints = false
+        mainObj.layer.cornerRadius = 20
+        return mainObj
+    }()
+    
+    //setup buyPremiumLabel
+    private lazy var premiumLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Купить премиум"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    
+    //setup stackView
+    private lazy var premiumStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 23
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    //setup crown image
+    private lazy var crownImageView: UIImageView = {
+        let mainImg = UIImageView()
+        mainImg.image = UIImage(named: "crown")
+        mainImg.translatesAutoresizingMaskIntoConstraints = false
+        mainImg.contentMode = .scaleAspectFill
+        return mainImg
+    }()
+    
+    //setup clearMainView
+    private lazy var clearMainView: UIView = {
+        let clearView = UIView()
+        clearView.backgroundColor = .clear
+        clearView.translatesAutoresizingMaskIntoConstraints = false
+        return clearView
+    }()
+    
+    //setup supportButton
+    private lazy var supportButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = AppColors.mediumGreen
+        button.layer.cornerRadius = 20
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // setup gesture
+    func setupTapGesture() {
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        clearMainView.addGestureRecognizer(tapGesture!)
+        tapGesture?.isEnabled = false
+        
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as? SettingsTableViewCell else {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        settingsCell.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.reuseId, for: indexPath) as? SettingsTableViewCell else {
             return UITableViewCell()
         }
-        let item = settingsCell[indexPath.row]
-        //cell.configure(item)
+        let item = settingsCell[indexPath.section]
+        
+        //rounded edges
+        if indexPath.section == 4 ? true: false {
+            let cornerRadius: CGFloat = 20.0
+            let maskPath = UIBezierPath(roundedRect: cell.bounds,
+                                        byRoundingCorners: [.bottomLeft, .bottomRight],
+                                        cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = maskPath.cgPath
+            cell.layer.mask = maskLayer
+        }
+        
+        cell.configure(contact: item)
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        70
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView()
+        footerView.backgroundColor = .clear
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5
+    }
+    //MARK: --Objc
+    @objc private func handleTap(_ gesture: UITapGestureRecognizer) {
+        let vc = HomeViewController()
+        present(vc, animated: true)
+    }
+    
 }
